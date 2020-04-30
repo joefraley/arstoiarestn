6b1d5f9010eeb2e2944a8ef760c3eff5e477c888
matthoiland <matthew.hoiland@cambiahealth.com>
2020-04-30 15:31:48 -0700

    Remove failing tests and reset pre-push hook

--- features/example.feature
+++ features/example.feature
@@ -7,7 +7,3 @@ Feature: Example
         Given I am on the home page
         When I click on Settings in the sidebar
         Then I will be on the settings page
-
-    Scenario: This should fail
-        Given this step definition exists
-        Then the scenario should fail
--- features/steps/example.steps.js
+++ features/steps/example.steps.js
@@ -20,10 +20,3 @@ Then("I will be on the settings page", async () => {
     const actualPath = await scope.context.page.url()
     expect(actualPath).to.equal(settingsPath)
 })
-
-Given("this step definition exists", async () => {
-    return true
-})
-Then("the scenario should fail", async () => {
-    expect(true).to.equal(false)
-})
