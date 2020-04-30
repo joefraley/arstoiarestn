4e5d9cf78d1cc570be8dfa40e73ba77b7c0a6f24
matthoiland <matthew.hoiland@cambiahealth.com>
2020-04-30 14:33:38 -0700

    Update puppeteer for linux chrome

--- features/support/world.js
+++ features/support/world.js
@@ -5,13 +5,14 @@ const { setWorldConstructor } = require("cucumber")
 const puppeteer = require("puppeteer")
 const scope = require("./scope")
 
-const World = function() {
+const World = function () {
     scope.driver = puppeteer
     scope.config = {
         headless: true,
         slowMo: 10,
         width: 1280,
         height: 1024,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"], // for CircleCI
     }
     scope.context = {}
 }
